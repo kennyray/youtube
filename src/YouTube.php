f6ba2aa9af00f25738553066712c73f82d73fb57
Kenny Ray <kenny@kennyray.com>
2016-03-29 23:27:20 -0400

    Added getVideoInfo method

--- src/YouTube.php
+++ src/YouTube.php
@@ -16,6 +16,16 @@ class YouTube
      */
     protected $youtube_api_key; 
 
+    /**
+     * @var string
+     */
+    protected $base_api_url = "https://www.googleapis.com/youtube/v3/";
+
+    //protected $part = ['id', 'snippet', 'contentDetails', 'player', 'status'];
+    protected $part = 'id,snippet,contentDetails,player,status';
+
+
+
      /**
      * Constructor
      * $youtube = new Youtube(array('key' => 'KEY HERE'))
@@ -34,6 +44,39 @@ class YouTube
             throw new \Exception('Google API key is Required, please visit https://console.developers.google.com/');
         }
     }
+
+
+    public function getVideoInfo($id)
+    {
+    	$params = array(
+ 			'id' => $id,
+ 			'key' => $this->youtube_api_key,
+ 			'part' => $this->part
+    	);
+    	$baseurl = $this->base_api_url . 'videos';
+    	return $this->getData($baseurl, $params);  // This will be extracted to separate class
+    }
+
+    protected function getData($baseurl, $params)
+    {	
+    	$query_string = http_build_query($params);
+
+    	$url = $baseurl . '?' . $query_string;
+    
+    	$ch = curl_init();  
+	    curl_setopt($ch,CURLOPT_URL,$url);
+	    curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
+	    $output=curl_exec($ch);
+	    curl_close($ch);
+
+	    return $output;
+    }
+
+
+
+
+
+
 
 	public function hello(){
 		return "Hello! Your API Key is " . $this->youtube_api_key;
